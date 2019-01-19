a360c1119ed3b490bf2aff7294a49f0287948d4a
qiaobilong <1031647261@qq.com>
2019-01-19 22:45:04 +0800

    1.处理评分bug 2.数据写入json

--- WebSpider/douban.py
+++ WebSpider/douban.py
@@ -3,14 +3,21 @@
 
 """豆瓣电影"""
 
-
 __author__ = 'Zerone龍'
-__version__ = 0.1
+__version__ = 1.0
 
 
-import time
 import requests
+import json
 from bs4 import BeautifulSoup
+
+
+def get_score(attrs):
+    """处理未评星的评论数据"""
+    if 'class' in attrs.keys():
+        return attrs['class'][0].replace('allstar', '')
+    else:
+        return '0'
 
 
 def get_data(url, offset):
@@ -26,13 +33,13 @@ def get_data(url, offset):
 
             # 获取每条评论数据
             for item in soup.select('.comment-item'):
-                yield{
+                # 对generator（生成器）还不是很理解，用yield应该是不用再将数据写入列表了
+                yield {
                     'movie_name': movie_name,
                     'user_id': item.input['value'],
                     'username': item.find(class_='comment-info').a.text,
                     'create_time': item.find(class_='comment-time')['title'],
-                    # 评分获取的时候经常会报错，暂时还不知道如何处理2019-1-18 22:29:52
-                    'score': item.find(class_='comment-time').find_previous_sibling()['class'][0].replace('allstar', ''),
+                    'score': get_score(item.find(class_='comment-time').find_previous_sibling().attrs),
                     'is_useful': item.find(class_='votes').text,
                     'comment': item.find(class_='short').text
                 }
@@ -43,7 +50,8 @@ def get_data(url, offset):
 def main(movie, offset):
     url = f'https://movie.douban.com/subject/{movie}/comments?start={offset}&limit=20&sort=new_score&status=P'
     for item in get_data(url, offset):
-        lst.append(item)
+        with open('douban.json', 'a', encoding='utf-8') as file:
+            file.write(json.dumps(item, ensure_ascii=False))
 
 
 headers = {
@@ -51,15 +59,14 @@ headers = {
 }
 # 电影的唯一标识，可以通过浏览器查看url获取
 movie_id = 26394152
+# 电影名
 movie_name = ''
+# 开始抓取的页码，0为第一页
 start = 0
-stop = 6
-lst = []
+# 抓取的截止页码
+stop = 2500
 
 if __name__ == '__main__':
-    pages = [x * 20 for x in range(start, stop+1)]
+    pages = [x * 20 for x in range(start, stop + 1)]
     for i in pages:
         main(movie_id, i)
-        time.sleep(0.3)
-    for i in lst:
-        print(i)
